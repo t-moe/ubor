8b9b7ecc314973cbfd8622d1ff23e686604a703e
Marcus <marcus.schluep@kns.ch>
2017-04-24 13:56:08 +0200

    removed different grap positions

--- src/arm.c
+++ src/arm.c
@@ -74,34 +74,25 @@ void vMoveRoboter(void *pvData) {
 
 	int leftRightSel = (int)pvData;
 	uint8_t *posArm;
-	uint8_t *timosWish;
 	int id_arm_comand_request;
-    int indexArmGripPosition = 0;
 
 	/* Init */
 	if(leftRightSel == 0)
 	{
-        static uint8_t posArmRight[11][8] = {
-			/*                      Arm	   B     S     E     H     G     x     x */
-			/*0 Nullposition     */ {0x02, 0x00, 0x19, 0x1A, 0x21, 0x01, 0x00, 0x00},
-			/*1 StartPosition    */ {0x02, 0x00, 0x1F, 0x1A, 0x21, 0x01, 0x00, 0x00},
-			/*2 Zp1 FB Links     */ {0x02, 0x00, 0x1F, 0x1A, 0x21, 0x00, 0x00, 0x00},
-			/*3 Zp2 FB Links     */ {0x02, 0x00, 0x19, 0x1A, 0x21, 0x00, 0x00, 0x00},
-			/*4 G offen FB Links */ {0x02, 0xee, 0x00, 0x40, 0x21, 0x00, 0x00, 0x00},
-			/*5 G zu FB Links    */ {0x02, 0xD2, 0x00, 0x2b, 0x21, 0x00, 0x00, 0x00},
-			/*6 Zp1 FB Mitte     */ {0x02, 0xD2, 0x1c, 0x1d, 0x21, 0x00, 0x00, 0x00},
-			/*7 Zp2 FB Mitte     */ {0x02, 0xD2, 0x1c, 0x1d, 0x21, 0x01, 0x00, 0x00},
-			/*8 G zu FB Mitte    */ {0x02, 0xD2, 0x13, 0x1d, 0x21, 0x01, 0x00, 0x00},
-			/*9 G offen FB Mitte */ {0x02, 0xD2, 0x00, 0x36, 0x21, 0x01, 0x00, 0x00},
-                                    {0x02, 0xEE, 0x00, 0x36, 0x21, 0x01, 0x00, 0x00},
+        static uint8_t posArmRight[11][6] = {
+			/*                      Arm	   B     S     E     H     G  */
+			/*0 Nullposition     */ {0x02, 0x00, 0x19, 0x1A, 0x21, 0x01},
+			/*1 StartPosition    */ {0x02, 0x00, 0x1F, 0x1A, 0x21, 0x01},
+			/*2 Zp1 FB Links     */ {0x02, 0x00, 0x1F, 0x1A, 0x21, 0x00},
+			/*3 Zp2 FB Links     */ {0x02, 0x00, 0x19, 0x1A, 0x21, 0x00},
+			/*4 G offen FB Links */ {0x02, 0xee, 0x00, 0x40, 0x21, 0x00},
+			/*5 G zu FB Links    */ {0x02, 0xD2, 0x00, 0x2b, 0x21, 0x00},
+			/*6 Zp1 FB Mitte     */ {0x02, 0xD2, 0x1c, 0x1d, 0x21, 0x00},
+			/*7 Zp2 FB Mitte     */ {0x02, 0xD2, 0x1c, 0x1d, 0x21, 0x01},
+			/*8 G zu FB Mitte    */ {0x02, 0xD2, 0x13, 0x1d, 0x21, 0x01},
+			/*9 G offen FB Mitte */ {0x02, 0xD2, 0x00, 0x36, 0x21, 0x01},
+                                    {0x02, 0xEE, 0x00, 0x36, 0x21, 0x01},
 			};
-        static uint8_t timosWishRight[5][8] = {
-                					{0x02, 0x00, 0x1b, 0x21, 0x10, 0x01, 0x00, 0x00}, // holen aussen
-                					{0x02, 0x00, 0x18, 0x25, 0x10, 0x01, 0x00, 0x00}, // holen aussen mitte
-                					{0x02, 0x00, 0x1a, 0x22, 0x18, 0x01, 0x00, 0x00}, // holen mitte
-                					{0x02, 0x00, 0x18, 0x24, 0x18, 0x01, 0x00, 0x00}, // holen innen mitte
-                					{0x02, 0x00, 0x18, 0x26, 0x18, 0x01, 0x00, 0x00}, // holen innen
-        							};
 
 		mutexMiddlePosition = xSemaphoreCreateBinary();
 		xSemaphoreGive(mutexMiddlePosition);
@@ -110,7 +101,6 @@ void vMoveRoboter(void *pvData) {
 		ucan_link_message_to_queue(ROBOT_R_STATUS_RETURN_ID, queueRobotRight);
 
 		posArm = (uint8_t *)posArmRight;
-		timosWish = (uint8_t *)timosWishRight;
 		id_arm_comand_request = ROBOT_R_COMAND_REQUEST_ID;
 
 		ucan_send_data(0, ROBOT_R_RESET_ID, 0);
@@ -118,30 +108,23 @@ void vMoveRoboter(void *pvData) {
 
 	if(leftRightSel == 1)
 	{
-        static uint8_t posArmLeft[11][8] = {
+        static uint8_t posArmLeft[11][6] = {
 				/*                      Arm	   B     S     E     H     G     x     x */
-	 									{0x02, 0x00, 0x19, 0x1A, 0x21, 0x01, 0x00, 0x00}, //Warte Position ECTS Abholen
-	 									{0x02, 0x00, 0x1F, 0x1A, 0x21, 0x01, 0x00, 0x00}, //ECTS Holen Mitte
-	 									{0x02, 0x00, 0x1F, 0x1A, 0x21, 0x00, 0x00, 0x00}, //ECTS Greifen
-	 									{0x02, 0x00, 0x19, 0x1A, 0x21, 0x00, 0x00, 0x00}, //ECTS Anheben
-	 									{0x02, 0x12, 0x00, 0x40, 0x21, 0x00, 0x00, 0x00}, //Drehen bis pos 1
-	 									{0x02, 0x2D, 0x00, 0x2b, 0x21, 0x00, 0x00, 0x00}, //Drehen bis pos 2 bereit zum ablegen
-	 									{0x02, 0x2D, 0x1c, 0x1d, 0x21, 0x00, 0x00, 0x00}, //ECTS Ablegen
-	 									{0x02, 0x2D, 0x1c, 0x1d, 0x21, 0x01, 0x00, 0x00}, //ECTS Ablegen
-	 									{0x02, 0x2D, 0x13, 0x1d, 0x21, 0x01, 0x00, 0x00}, //Arm auserhalb ects
-	 									{0x02, 0x2D, 0x00, 0x36, 0x21, 0x01, 0x00, 0x00}, //Arm ausserhalb mitte
-                                        {0x02, 0x12, 0x00, 0x36, 0x21, 0x01, 0x00, 0x00}, //Arm ausserhalb mitte
+	 									{0x02, 0x00, 0x19, 0x1A, 0x21, 0x01}, //Warte Position ECTS Abholen
+	 									{0x02, 0x00, 0x1F, 0x1A, 0x21, 0x01}, //ECTS Holen Mitte
+	 									{0x02, 0x00, 0x1F, 0x1A, 0x21, 0x00}, //ECTS Greifen
+	 									{0x02, 0x00, 0x19, 0x1A, 0x21, 0x00}, //ECTS Anheben
+	 									{0x02, 0x12, 0x00, 0x40, 0x21, 0x00}, //Drehen bis pos 1
+	 									{0x02, 0x2D, 0x00, 0x2b, 0x21, 0x00}, //Drehen bis pos 2 bereit zum ablegen
+	 									{0x02, 0x2D, 0x1c, 0x1d, 0x21, 0x00}, //ECTS Ablegen
+	 									{0x02, 0x2D, 0x1c, 0x1d, 0x21, 0x01}, //ECTS Ablegen
+	 									{0x02, 0x2D, 0x13, 0x1d, 0x21, 0x01}, //Arm auserhalb ects
+	 									{0x02, 0x2D, 0x00, 0x36, 0x21, 0x01}, //Arm ausserhalb mitte
+                                        {0x02, 0x12, 0x00, 0x36, 0x21, 0x01}, //Arm ausserhalb mitte
 				};
-        static uint8_t timosWishLeft[5][8] = {
-        								{0x02, 0x00, 0x17, 0x24, 0x1e, 0x01, 0x00, 0x00}, // holen innen
-        								{0x02, 0x00, 0x19, 0x21, 0x1e, 0x01, 0x00, 0x00}, // holen innen mitte
-        								{0x02, 0x00, 0x1d, 0x1a, 0x21, 0x01, 0x00, 0x00}, // holen mitte
-        								{0x02, 0x00, 0x1d, 0x1a, 0x1e, 0x01, 0x00, 0x00}, // holen aussen mitte
-        								{0x02, 0x00, 0x1f, 0x15, 0x21, 0x01, 0x00, 0x00}, // holen aussen
-        };
+
 	 	posArm = (uint8_t *)posArmLeft;
-	 	timosWish = (uint8_t *)timosWishLeft;
-	 	id_arm_comand_request = ROBOT_L_COMAND_REQUEST_ID;
+	  	id_arm_comand_request = ROBOT_L_COMAND_REQUEST_ID;
 
 	 	queueRobotLeft = xQueueCreate(MSG_QUEUE_SIZE, sizeof(CARME_CAN_MESSAGE));
 	 	ucan_link_message_to_queue(ROBOT_L_STATUS_RETURN_ID, queueRobotLeft);
@@ -158,6 +141,12 @@ void vMoveRoboter(void *pvData) {
 		{
             display_log(DISPLAY_NEWLINE, "Going to position %u",n);
 
+            //before we want to grab the block
+         if(n==1){
+			int8_t pos;
+			pos = bcs_grab(leftRightSel == 1 ? belt_left : belt_right);
+			display_log(DISPLAY_NEWLINE,"block is at pos %d",pos);
+         }
 
 			if(n == 6){ //before we want to access the mid position
                 display_log(DISPLAY_NEWLINE, "take semaphore mid");
@@ -169,35 +158,9 @@ void vMoveRoboter(void *pvData) {
             }
 
 
-			if(n == 1 || n==2) { //before we want to grab the block
-				if(n==1){
-                    int8_t pos;
-                    pos = bcs_grab(leftRightSel == 1 ? belt_left : belt_right);
-                    display_log(DISPLAY_NEWLINE,"block is at pos %d",pos);
-                    if(pos <= -14){
-                        indexArmGripPosition = 0;
-                    }
-                    else if(pos >= -15 && pos <= 1){
-                        indexArmGripPosition = 1;
-                    }
-                    else if(pos >= 2 && pos <= 17){
-                        indexArmGripPosition = 2;
-                    }
-                    else if(pos >= 18 && pos <= 34){
-                        indexArmGripPosition = 3;
-                    }
-                    else if(pos >= 35){
-                        indexArmGripPosition = 4;
-                    }
-                }
-				//grab block
-                timosWish[indexArmGripPosition*8+5] = (n==1) ? 1 : 0;
-                ucan_send_data(COMAND_DLC, id_arm_comand_request, &timosWish[indexArmGripPosition*8] );
-                waitUntilPos(&timosWish[indexArmGripPosition*8], leftRightSel);
-			} else {  //we dont want to grab a block -> go to position
-				ucan_send_data(COMAND_DLC, id_arm_comand_request, &posArm[n*8] );
-				waitUntilPos(&posArm[n*8], leftRightSel);
-			}
+
+				ucan_send_data(COMAND_DLC, id_arm_comand_request, &posArm[n*6] );
+				waitUntilPos(&posArm[n*6], leftRightSel);
 
             if(n==3) { //after we picked up a block
                 bcs_signal_band_free(leftRightSel == 1 ? belt_left : belt_right);
@@ -265,7 +228,7 @@ void waitUntilPos(uint8_t *pos, int side)
 
 		//display_log(DISPLAY_NEWLINE, "Left arm reached position");
 	}
-    vTaskDelay(200);
+    vTaskDelay(1000);
 }
 
 void init_arm()
@@ -284,8 +247,8 @@ void init_arm()
 	                (void*)0,
 	                ARM_TASK_PRIORITY,
 	                NULL);
-
-    /*xTaskCreate(vManualArmMovment,
+/*
+    xTaskCreate(vManualArmMovment,
 				"Manual Arm Movement",
 				ARM_TASK_STACKSIZE,
 				NULL,
@@ -311,7 +274,7 @@ void vManualArmMovment(void *pvData)
 	uint8_t buttonData;
 	uint8_t switchData;
 
-	uint8_t posManuel[8] = {0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	uint8_t posManuel[6] = {0x02, 0x00, 0x00, 0x00, 0x00, 0x00};
 	bool increment = false;
 	bool leftSelect = false;
 
