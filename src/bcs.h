440daed1b3d2fb0993b2d611275ce382b8a1ccf9
T-Moe <timolang@gmail.com>
2017-05-01 11:05:25 +0200

    Doc update

--- src/bcs.h
+++ src/bcs.h
@@ -2,11 +2,16 @@
 #define BCS_H
 #include <stdint.h>
 
-//Enum to destinguish the different tasks. The members point to the base address of the can endpoints
-enum belt_select {belt_left=0x110,
-                  belt_mid=0x120,
-                  belt_right=0x130};
+/**
+ * @brief The belt_select enum differenciates between the different belt tasks. The members point to the base address of the can endpoints
+ */
+enum belt_select {belt_left=0x110, //!< the left belt
+                  belt_mid=0x120, //!< the middle belt
+                  belt_right=0x130//!< the right belt
+                 };
 
+
+//doc see bcs.c
 int8_t bcs_grab(enum belt_select belt);
 void bcs_prepare_drop(enum belt_select belt);
 void bcs_signal_dropped(enum belt_select belt);
